2be2b0ba138bea80a8c115231c7d9951f4d1e7eb
Edmund Xin <edmundxin02@gmail.com>
2024-01-08 16:51:27 -0800

    Add initial OpenGL code to display blank window

--- minecraft-at-home/src/main.c
+++ minecraft-at-home/src/main.c
@@ -1,11 +1,57 @@
+#include "glad/glad.h"
 #include <GLFW/glfw3.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+void error_callback(int error, const char* description) {
+  fprintf(stderr, "Error: %s\n", description);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, GLFW_TRUE);
+  }
+}
 
 int main() {
-  printf("Hello world\n");
-
   if (!glfwInit()) {
-    printf("GLFW initialization failed");
-    return 1;
+    printf("Failed to initialize GLFW");
+    exit(EXIT_FAILURE);
   }
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwSetErrorCallback(error_callback);
+
+  // Initialization
+  GLFWwindow* window = glfwCreateWindow(640, 480, "Totally Minecraft", NULL, NULL);
+  if (!window) {
+    printf("Failed to initialize window");
+    glfwTerminate();
+    exit(EXIT_FAILURE);
+  }
+
+  glfwMakeContextCurrent(window);
+  glfwSetKeyCallback(window, key_callback);
+  glfwSwapInterval(1);
+
+  // Initialize GLAD
+  if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+    printf("Failed to initialize GLAD");
+  }
+
+  // Initialization finished, program loop
+  while (!glfwWindowShouldClose(window)) {
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+  
+  // Clean up
+  glfwDestroyWindow(window);
+  glfwTerminate();
 }
